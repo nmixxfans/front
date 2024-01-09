bef2b6d1300e8ff7347a31b59f45b8f414051fd8
Leewonno <dldnjssh123@naver.com>
2024-01-09 19:52:54 +0900

    [Feat] 건의 페이지 디자인

--- src/app/send/page.tsx
+++ src/app/send/page.tsx
@@ -1,7 +1,28 @@
+import send from "../css/send.module.css";
+
 export default function Send(){
     return(
-        <>
-            건의페이지
-        </>
+        <section className={send.section}>
+            <div className={send.container}>
+                <div className={send.titleBox}>
+                    <div className={send.title}>건의</div>
+                </div>
+                <div className={send.contentBox}>
+                    <div className={send.noticeBox}>
+
+                    </div>
+                    <div className={send.sendBox}>
+                        <div className={send.categoryBox}>
+
+                        </div>
+                        <div className={send.content}>
+                            <textarea>
+                                
+                            </textarea>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
