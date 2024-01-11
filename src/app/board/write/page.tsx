dce33106776c846772178509b9bd6aa1b6ec6fb1
Leewonno <dldnjssh123@naver.com>
2024-01-12 05:14:18 +0900

    [Feat] 클 쓰기 페이지 틀잡기

--- src/app/board/write/page.tsx
+++ src/app/board/write/page.tsx
@@ -1,7 +1,24 @@
+import bw from "../../css/boardWrite.module.css";
+
 export default function BoardWrite() {
     return (
-        <>
-            자유게시판 글쓰기
-        </>
+        <section className={bw.section}>
+            <div className={bw.container}>
+                <div className={bw.informationBox}>
+                    <div className={bw.horseHeadBox}>
+                        <input type="radio" name="horseHead" id="n" />
+                        <label htmlFor="n">일반</label>
+                    </div>
+                    <div className={bw.titleBox}>
+
+                    </div>
+                </div>
+                <div className={bw.editorBox}>
+                    <div className={bw.editor} contentEditable="true">
+
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
