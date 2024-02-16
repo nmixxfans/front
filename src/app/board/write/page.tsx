5580487f5611072b54b862412f494d7caad20e5f
Leewonno <dldnjssh123@naver.com>
2024-02-16 10:50:16 +0900

    [Add] react-uid 패키지 추가

--- src/app/board/write/page.tsx
+++ src/app/board/write/page.tsx
@@ -1,39 +1,46 @@
 "use client"
 
-import { useState } from "react";
+import { useRef, useState } from "react";
+import ContentEditable from "react-contenteditable";
 import bw from "../../css/boardWrite.module.css";
 
 export default function BoardWrite() {
 
-    const [title, setTitle] = useState<string>("");
+  const [title, setTitle] = useState<string>("");
+  const [state, setState] = useState({ html: "", tagName: "div" });
+  const ref = useRef<HTMLDivElement>(null);
 
-    return (
-        <section className={bw.section}>
-            <div className={bw.container}>
-                <div className={bw.informationBox}>
-                    <div className={bw.horseHeadBox}>
-                        <input type="radio" name="horseHead" id="n" />
-                        <label htmlFor="n">일반</label>
-                    </div>
-                    <div className={bw.titleBox}>
-                        <input className={bw.title} value={title} onChange={(e)=>setTitle(e.target.value)} placeholder="제목" />
-                    </div>
-                </div>
-                <div className={bw.editorBox}>
-                    <div className={bw.editorBtnBox}>
+  return (
+    <section className={bw.section}>
+      <div className={bw.container}>
+        <div className={bw.informationBox}>
+          <div className={bw.horseHeadBox}>
+            <input type="radio" name="horseHead" id="n" />
+            <label htmlFor="n">일반</label>
+          </div>
+          <div className={bw.titleBox}>
+            <input className={bw.title} value={title} onChange={(e) => setTitle(e.target.value)} placeholder="제목" />
+          </div>
+        </div>
+        <div className={bw.editorBox}>
+          <div className={bw.editorBtnBox}>
 
-                    </div>
-                    <div className={bw.editor} contentEditable="true">
-                        
-                    </div>
-                </div>
-                <div className={bw.noticeBox}>
-                    안내사항
-                </div>
-                <div className={bw.saveBox}>
-                    <div className={bw.saveBtn}>저장</div>
-                </div>
-            </div>
-        </section>
-    )
+          </div>
+          <ContentEditable
+            innerRef={ref}
+            className={bw.editor}
+            html={state.html}
+            disabled={false}
+            onChange={(e) => setState((prev) => ({ ...prev, html: e.target.value }))}
+             />
+        </div>
+        <div className={bw.noticeBox}>
+          안내사항
+        </div>
+        <div className={bw.saveBox}>
+          <div className={bw.saveBtn}>저장</div>
+        </div>
+      </div>
+    </section>
+  )
 }
