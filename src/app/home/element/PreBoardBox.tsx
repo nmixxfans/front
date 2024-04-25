83199a52ed8ccbc493216c1849f824cef87d2973
Leewonno <dldnjssh123@naver.com>
2024-04-25 18:23:56 +0900

    [Feat] 서버 컴포넌트 변경

---
+++ src/app/home/element/PreBoardBox.tsx
@@ -0,0 +1,39 @@
+import styles from '../home-element.module.css';
+import Link from 'next/link';
+import { korTime } from '../../asset/functions/utc-to-kor';
+
+interface BoardItem{
+    id:number,
+    title:string,
+    create_date:string,
+}
+
+interface Props{
+    board:BoardItem[]
+}
+
+export default function PreBoardBox({board}:Props){
+    return(
+        <div className={styles.secondBoxItem}>
+          <div className={styles.title} title='자유게시판'>자유게시판</div>
+          <div className={[styles.secondBoxContent, styles.boxScroll].join(" ")}>
+            {
+              board.map((value, index) => {
+                return (
+                  <div className={styles.boardItem} key={index}>
+                    <div className={styles.boardTitle}>
+                      <Link href={`/board/${value.id}`} className={styles.boardTitleLink}>
+                        {value.title}
+                      </Link>
+                    </div>
+                    <div className={styles.boardDate}>
+                      {korTime(new Date(value.create_date))}
+                    </div>
+                  </div>
+                )
+              })
+            }
+          </div>
+        </div>
+    )
+}
